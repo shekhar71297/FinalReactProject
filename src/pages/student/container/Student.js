e3ae57e088a5226c4fe4cbca902add1ee9983e42
Sujit Gaikwad <sujitg1313@gmail.com>
2023-08-02 15:48:32 +0530

    folder struct created in src

---
+++ src/pages/student/container/Student.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import NavBar from '../../../component/student/NavBar'
+
+export class Student extends Component {
+    render() {
+        return (
+            <div>
+                <NavBar/>
+            </div>
+        )
+    }
+}
+
+export default Student

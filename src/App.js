baba480b9f4ab180b9381b448b70017c48519f83
ompacharne <ompacharne456@gmail.com>
2023-08-04 16:30:50 +0530

    changed file name

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import './App.css';
-<<<<<<< HEAD
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Sidenav from './pages/Sidenav';
+import Questiontable from './components/question/Questiontable';
 import Addform from './components/question/Addform';
 import Header from './components/question/Header';
-import Questiontable from './components/question/Questiontable'
-
-
-=======
-import Student from './pages/student/container/Student';
->>>>>>> 732659d9b11ea6630c5023e712eb9c6d94832829
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      
       <Header/>
       <Addform/>
       <Questiontable/>
+      <Sidenav/>
       
       
       
-=======
-    <Student/>
->>>>>>> 732659d9b11ea6630c5023e712eb9c6d94832829
+      
+      
+      
+      
+      
+    
     </div>
   );
 }

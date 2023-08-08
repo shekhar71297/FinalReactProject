28b4d2b1c86ef84b25bcf188c0896b92945088b3
Sujit Gaikwad <sujitg1313@gmail.com>
2023-08-08 18:31:01 +0530

    fixes for redux merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
-import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Sidenav from './pages/Sidenav';
-import Sidenavbar from './pages/Sidenavbar';
+// import Sidenavbar from './pages/Sidenavbar';
 
-import Student from './pages/student/container/Student';
+// import Student from './pages/student/container/Student';
 import NewStudentRegistration from './component/student/NewStudentRegistration'
 import StudentDashboard from './component/student/StudentDashboard';
 import NavBar from './component/student/NavBar';
+import store from './store/store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <div className="App">
-    <BrowserRouter>
-    {/* <Student/> */}
-    
-    <Routes>
-      <Route path='/' element={<NavBar/>}/>
-      <Route path='Register' element={<NewStudentRegistration />}/>
-    </Routes>
-    <NewStudentRegistration/>
-    <StudentDashboard/>
-    </BrowserRouter>
-    {/* <Student /> */}
-   
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <BrowserRouter>
+          {/* <Student/> */}
+          <Routes>
+            <Route path='/' element={<NavBar />} />
+            <Route path='Register' element={<NewStudentRegistration />} />
+            <Route path='dash' element={<StudentDashboard />} />
+          </Routes>
+          {/* <NewStudentRegistration/> */}
+          
+          {/* <Sidenav/> */}
+        </BrowserRouter>
+        {/* <Student /> */}
+
+      </div>
+    </Provider>
+
   );
 }
 

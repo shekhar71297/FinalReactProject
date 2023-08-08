2a5c1a32af0e9dccd3076fbdef5c0ecbd2f695c4
shekhar <shekhar.powar@oulook.com>
2023-08-08 18:43:39 +0530

    revert testing

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,7 +2,7 @@ import {combineReducers} from 'redux'
 import StudentReducer from '../pages/student/Reducer'; 
 
 const rootReducer = combineReducers({
-        StudentStore:StudentReducer ,
+        
         
     });
     

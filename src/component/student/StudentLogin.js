1f58bf681423dcbfa22ec885bc404640dceed0d0
Sujit Gaikwad <sujitg1313@gmail.com>
2023-08-09 17:21:54 +0530

    Add student login & reducer

--- src/component/student/StudentLogin.js
+++ src/component/student/StudentLogin.js
@@ -12,119 +12,123 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { hover } from '@testing-library/user-event/dist/hover';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios'
-import { Link } from 'react-router-dom';
-
+import axios from 'axios';
+import { Link } from "react-router-dom";
+import WithRouter from '../../util/WithRouter';
 
 const defaultTheme = createTheme();
+export class StudentLogin extends Component {
+  constructor(props) {
+    super(props)
 
-
-const StudentLogin = () => {
-  const navigate=useNavigate();
-  const[students,setStudent]=useState({
-      email:"",
-      dob:""
-    });
-
-
-    const inputChangeHandler=(event)=>{
-      setStudent({...students,[event.target.name]:event.target.value});
-  }
-  
-  const checkData =(event)=>{
-      event.preventDefault();
-      axios.get("http://localhost:8888/students").then((res) => {
-  
-                  const data =res.data.filter((item)=>{return item.email===students.email && item.dob===students.dob})
-
-                  if(data.length > 0){
-                      console.log(students.email,students.dob);                                   
-                      sessionStorage.setItem("islogin",students.email);
-                      navigate("/Register");
-                  }else{
-                      window.alert("wrong credential");
-                      setStudent({email:"",dob:""});
-                  }
-          });
-      
+    this.state = {
+      email: "",
+      dob: ""
+    }
   }
 
- 
-  return (
-    <div>
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+  inputChangeHandler = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value })
+  }
+  submitBtn = (e) => {
+    e.preventDefault()
+    axios.get("http://localhost:8888/students", this.state).then((res) => {
+      const istrue = res.data.some((d) =>
+        this.state.email === d.email && this.state.dob === d.dob
+      )
+      if (istrue) {
+        sessionStorage.setItem("isLogin", "true")
 
-          }}
-        >
-          <Typography component="h1" variant="h5">
-            Student Login
-          </Typography>
-          <Box component="form" onSubmit={checkData} marginTop={3} padding={3} boxShadow={'5px 5px 10px #ccc'} sx={{  ":hover": {
-              boxShadow:'10px 10px 20px #ccc'
-            }}} >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Enter Email/Username"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              value={students.email}
-              onChange={inputChangeHandler}
-            />
-            <TextField  
-              margin="normal"
-              required
-              fullWidth
-              name="dob"
-              type="date"
-              id="dob"
-              value={students.dob}
-              onChange={inputChangeHandler}
-              
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              // onClick={()=>setIsLogin(!isLogin)}
+        // alert("Login Successfully")
+        window.alert("Login successfully");
+        // <Link to='/cat'/>
+        // this.props.router.navigate('/voucher')
+
+
+      } else {
+        window.alert("please cheack your register email and dob")
+      }
+    })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <ThemeProvider theme={defaultTheme}>
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+              sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+
+              }}
             >
-              Login In
-            </Button>
-            <Grid container style={{ textAlign: 'left' }}>
-              <Grid item xs>
-                <Link to='/Register' variant="body2" style={{ color: 'red', textDecoration: 'none' }}>
-                  New Student ? Register Here
-                </Link><br />
-                <Link href="#" variant="body2" style={{ color: 'red', textDecoration: 'none' }}>
-                  Feedback ? Click Here
-                </Link>
-              </Grid>
-              <br />
-            </Grid>
+              <Typography component="h1" variant="h5">
+                Student Login
+              </Typography>
+              <Box component="form" onSubmit={checkData} marginTop={3} padding={3} boxShadow={'5px 5px 10px #ccc'} sx={{
+                ":hover": {
+                  boxShadow: '10px 10px 20px #ccc'
+                }
+              }} >
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Enter Email/Username"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  value={students.email}
+                  onChange={inputChangeHandler}
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="dob"
+                  type="date"
+                  id="dob"
+                  value={students.dob}
+                  onChange={inputChangeHandler}
+
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                // onClick={()=>setIsLogin(!isLogin)}
+                >
+                  Login In
+                </Button>
+                <Grid container style={{ textAlign: 'left' }}>
+                  <Grid item xs>
+                    <Link to='/Register' variant="body2" style={{ color: 'red', textDecoration: 'none' }}>
+                      New Student ? Register Here
+                    </Link><br />
+                    <Link href="#" variant="body2" style={{ color: 'red', textDecoration: 'none' }}>
+                      Feedback ? Click Here
+                    </Link>
+                  </Grid>
+                  <br />
+                </Grid>
+              </Box>
             </Box>
-          </Box>
-        
-        </Container>
-      </ThemeProvider>
-      {/* { istrue && <Voucher/> } */}
-            </div>
-        )
-    
+
+          </Container>
+        </ThemeProvider>
+        {/* { istrue && <Voucher/> } */}
+      </div>
+    )
+  }
+
 }
 
 export default WithRouter(StudentLogin)

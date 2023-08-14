b2a672d140f79903880bebbe7830ea4e8c80bc20
ompacharne <ompacharne456@gmail.com>
2023-08-14 13:54:16 +0530

    changed name

--- src/pages/question/container/Question.js
+++ src/pages/question/container/Question.js
@@ -1,25 +1,27 @@
 import React from 'react'
 import Questiontable from '../../../components/question/Questiontable'
 import Addform from '../../../components/question/Addform'
-import { connect } from 'react-redux'
-import * as Action from '../../question/Action'
+import Popup from '../../../components/question/Popup'
 
 
-const Question = () => {
+
+const QuestionMain = () => {
     return (
         <div>
-            <Questiontable/>
-            <Addform/>
+          <Addform/>
+          <Popup/>
+          <Questiontable/>
+            
             
         </div>
     )
 }
-const  mapStateToProps = (state) =>({
-    allquestions: state.questionStore.allquestions
-  })
-  const  mapDispatchToProps = (dispatch) =>({
-    initquestionRequest: ()=> dispatch(Action.getAllQuestions()),
-    addQuestionRequest: (data)=> dispatch(Action.addAllQuestions(data))
-  })
+// const  mapStateToProps = (state) =>({
+//     allquestions: state.questionStore.allquestions
+//   })
+//   const  mapDispatchToProps = (dispatch) =>({
+//     initquestionRequest: ()=> dispatch(Action.getAllQuestions()),
+//     addQuestionRequest: (data)=> dispatch(Action.addAllQuestions(data))
+//   })
 
-export default connect(mapStateToProps, mapDispatchToProps) (Question);
+export default QuestionMain;

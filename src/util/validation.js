dc8a72d4a5cede5d8a1ce2888060b3782c5a1d69
Sujit Gaikwad <sujitg1313@gmail.com>
2023-08-19 19:16:10 +0530

    Applied Validations

--- src/util/validation.js
+++ src/util/validation.js
@@ -1,1 +1,3 @@
 export const isValidName = (name) => /^[a-zA-Z]{5,10}$/.test(name);
+export const isValidEmail = (email) => /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+export const isValidContact =(contact) => /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(contact);

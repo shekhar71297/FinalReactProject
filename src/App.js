4b4788fba9713740b43d4bddf38488933b5f94e5
Sujit Gaikwad <sujitg1313@gmail.com>
2023-08-20 11:59:11 +0530

    solved merge conflicts in appjs and student module

--- src/App.js
+++ src/App.js
@@ -29,6 +29,12 @@ function App() {
               <Route path='user' element={<User />} />
             </Route>
           </Routes>
+<<<<<<< Updated upstream
+=======
+          {/* <NewStudentRegistration/> */}
+          
+          <Sidenav/>
+>>>>>>> Stashed changes
         </BrowserRouter>
       </div>
     </Provider>
--- src/component/student/StudentDashboard.js
+++ src/component/student/StudentDashboard.js
@@ -214,12 +214,6 @@ class StudentDashboard extends Component {
   }
 
 
-  // handleChange = (event) => {
-  //   // this.setState({
-  //     // [event.target.name]: event.target.value
-  //   // });
-
-
   handleChange = (event) => {
     const { name, value } = event.target;
 

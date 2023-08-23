3d0d800e4ab621ffd5cd374cf4e2d7b728e431bf
rushi <rushikakdeforever@gmail.com>
2023-08-23 18:26:03 +0530

    added exam module

--- src/util/validation.js
+++ src/util/validation.js
@@ -6,6 +6,7 @@ export const isValidName = (name) => /^[a-zA-Z]{2,10}$/.test(name);
 export const isValidContact = (contact) => /^[6789]\d{9}$/.test(contact);
 export const isValidEmail=(email)=>/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,15}$/.test(email);
 export const isValidPassword=(Password)=>/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(Password);
+export const isValidexamcode =(examcode)=> /^[a-zA-Z0-9]+\-+[a-zA-Z0-9\- ]{1,9}$/.test(examcode);
 export const errorText = (message) => {
     return (<span style={{'display':'flex','alignItems':'center'}}>
       <ErrorOutlineIcon /> <span style={{'paddingLeft':'5px'}}>{message}</span>
